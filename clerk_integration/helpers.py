8edf452cf5957091b9d77e2b5de07378652f2ed2
mitanshubhatt <mitanshubhatt@gofynd.com>
2025-05-14 12:56:44 +0530

    Configure org membership

--- clerk_integration/helpers.py
+++ clerk_integration/helpers.py
@@ -1,27 +1,10 @@
-from typing import Optional, List, Dict
+from typing import Optional, List
 from clerk_backend_api import Clerk
 
 import aiohttp
 
 
-class ClerkClientInterface:
-    async def get_clerk_users_by_id(self, user_ids: List[str]) -> List[Dict]:
-        pass
-
-    async def update_organization_metadata(self, organization_id: str, public_metadata: Optional[dict], private_metadata: Optional[dict]) -> bool:
-        pass
-
-    async def update_user_metadata(self, user_id: str, public_metadata: Optional[dict], private_metadata: Optional[dict]) -> bool:
-        pass
-
-    async def get_org_members(self, organization_id: str, query: Optional[str], limit: int, offset: int, user_id: Optional[str]) -> Dict:
-        pass
-
-    async def update_organisation_config(self, organization_id: str, max_allowed_memberships: int) -> bool:
-        pass
-
-
-class ClerkHelper(ClerkClientInterface):
+class ClerkHelper:
     """
     A class for managing Clerk authentications
     """
